7dd5e33d75b993ddf90e654a322e7125cec22b39
SML\gburgos <gustavo.burgos@chilecompra.cl>
2023-09-01 12:20:25 -0400

    initial commit

---
+++ prueba-sermaluc/src/test/java/cl/pruebasermaluc/PruebaSermalucApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.pruebasermaluc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSermalucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
